c32baecfab5af55731a4c114e1aab3b435310054
9adi3 <ahmadi.9adi3@gmail.com>
2025-06-20 10:50:44 +0100

    Update estore_items.js

--- estore_items.js
+++ estore_items.js
@@ -1,19 +1,21 @@
 fetch('products.json')
 .then(response => response.json())
 .then(data => {
-    console.log(data);
+    const cart = JSON.parse(localStorage.getItem('cart')) || []
 
     const swiper_items_sale = document.getElementById("swiper_items_sale")
 
     const swiper_electronics = document.getElementById("swiper_electronics")
 
-    const swiper_applinaces = document.getElementById("swiper_applinaces")
+    const swiper_appliances = document.getElementById("swiper_appliances")
 
     const swiper_mobiles = document.getElementById("swiper_mobiles")
 
 
     data.forEach(product => {
         if(product.old_price){
+
+            const isInCart =cart.some(cartItem => cartItem.id === product.id)
 
             const percent_disc =Math.floor((product.old_price - product.price) / product.old_price * 100)
                         
@@ -41,8 +43,12 @@ fetch('products.json')
                     </div>
                         
                     <div class="icons">
-                        <span class="btn_add_cart"><i class="fa-solid fa-cart-shopping"></i> Add to cart</span>
-                        <span class="icon_product"><i class="fa-regular fa-heart"></i></span>
+                        <span class="btn_add_cart ${isInCart ? 'active' : ''}" data-id="${product.id}">
+                            <i class="fa-solid fa-cart-shopping"></i>  ${isInCart ? 'Item in cart' : 'Add to cart'}
+                        </span>
+                        <span class="icon_product">
+                            <i class="fa-regular fa-heart"></i>
+                        </span>
                     </div>
                 </div>
                 `
@@ -52,6 +58,8 @@ fetch('products.json')
     data.forEach(product => {
         if(product.catetory == "electronics"){
             
+            const isInCart =cart.some(cartItem => cartItem.id === product.id)
+
             const old_price_Prgraph = product.old_price ? `<p class="old_price">$${product.old_price}</p>`:"";
             
             const precent_disc_div = product.old_price ? `<span class="sale_present">%${Math.floor((product.old_price - product.price) / product.old_price * 100)}</span>`:"";
@@ -81,8 +89,12 @@ fetch('products.json')
                     </div>
                         
                     <div class="icons">
-                        <span class="btn_add_cart"><i class="fa-solid fa-cart-shopping"></i> Add to cart</span>
-                        <span class="icon_product"><i class="fa-regular fa-heart"></i></span>
+                        <span class="btn_add_cart ${isInCart ? 'active' : ''}" data-id="${product.id}">
+                            <i class="fa-solid fa-cart-shopping"></i>  ${isInCart ? 'Item in cart' : 'Add to cart'}
+                        </span>
+                        <span class="icon_product">
+                            <i class="fa-regular fa-heart"></i>
+                        </span>
                     </div>
                 </div>
             `
@@ -91,13 +103,15 @@ fetch('products.json')
 
     data.forEach(product => {
         if(product.catetory == "appliances"){
-            
+ 
+            const isInCart =cart.some(cartItem => cartItem.id === product.id)
+
             const old_price_Prgraph = product.old_price ? `<p class="old_price">$${product.old_price}</p>`:"";
             
             const precent_disc_div = product.old_price ? `<span class="sale_present">%${Math.floor((product.old_price - product.price) / product.old_price * 100)}</span>`:"";
 
 
-            swiper_applinaces.innerHTML += `
+            swiper_appliances.innerHTML += `
                 <div class="swiper-slide product">
                     
 
@@ -121,57 +135,74 @@ fetch('products.json')
                     </div>
                         
                     <div class="icons">
-                        <span class="btn_add_cart"><i class="fa-solid fa-cart-shopping"></i> Add to cart</span>
-                        <span class="icon_product"><i class="fa-regular fa-heart"></i></span>
+                        <span class="btn_add_cart ${isInCart ? 'active' : ''}" data-id="${product.id}">
+                            <i class="fa-solid fa-cart-shopping"></i>  ${isInCart ? 'Item in cart' : 'Add to cart'}
+                        </span>
+                        <span class="icon_product"><i class="fa-regular fa-heart"></i>
+                        </span>
                     </div>
                 </div>
             `
         }
     })    
 
+
     data.forEach(product => {
-        if(product.catetory == "mobiles"){
-            
-            const old_price_Prgraph = product.old_price ? `<p class="old_price">$${product.old_price}</p>`:"";
-            
-            const precent_disc_div = product.old_price ? `<span class="sale_present">%${Math.floor((product.old_price - product.price) / product.old_price * 100)}</span>`:"";
+        if(product.catetory == "mobiles"){  
+
+            const isInCart = cart.some(cartItem => cartItem.id === product.id)
+
+            const old_price_Pargrahp = product.old_price ? `<p class="old_price">$${product.old_price}</p>` : "";
+
+            const percent_disc_div = product.old_price ? `<span class="sale_present">%${Math.floor((product.old_price - product.price) / product.old_price * 100)}</span>` : "";
+
 
 
             swiper_mobiles.innerHTML += `
-                <div class="swiper-slide product">
-                    
 
-                    <div class="img_product">
-                        <a href=""><img src="${product.img}" alt="Product"></a>
-                    </div>
-                        
-                    <div class="stars">
-                            <i class="fa-solid fa-star"></i>
-                            <i class="fa-solid fa-star"></i>
-                            <i class="fa-solid fa-star"></i>
-                            <i class="fa-solid fa-star"></i>
-                            <i class="fa-solid fa-star"></i>
-                    </div>
 
-                    <p class="name_product"><a href="">${product.name}</a></p>
+            <div class="swiper-slide product">
+                       
+                        ${percent_disc_div}
+                       <div class="img_product">
+                           <a href="#"><img src="${product.img}" alt=""></a>
+                       </div>
 
-                    <div class="price">
-                            <p><span>$${product.price}</span></p>
+                       <div class="stars">
+                           <i class="fa-solid fa-star"></i>
+                           <i class="fa-solid fa-star"></i>
+                           <i class="fa-solid fa-star"></i>
+                           <i class="fa-solid fa-star"></i>
+                           <i class="fa-solid fa-star"></i>
+                       </div>
 
-                    </div>
-                        
-                    <div class="icons">
-                        <span class="btn_add_cart"><i class="fa-solid fa-cart-shopping"></i> Add to cart</span>
-                        <span class="icon_product"><i class="fa-regular fa-heart"></i></span>
-                    </div>
-                </div>
-            `
+                       <p class="name_product"><a href="#">${product.name}</a></p>
+
+                       <div class="price">
+                           <p><span>$${product.price}</span></p>
+                           ${old_price_Pargrahp}
+                       </div>
+
+                       <div class="icons">
+                            <span class="btn_add_cart ${isInCart ? 'active' : ''}" data-id="${product.id}">
+                            <i class="fa-solid fa-cart-shopping"></i>  ${isInCart ? 'Item in cart' : 'Add to cart'}
+                            </span>
+                            <span class="icon_product"><i class="fa-regular fa-heart"></i>
+                            </span>
+                       </div>
+                   </div>
+           
+           
+           
+           
+           `
+
+
+
         }
-    }) 
-
+    })
+    
 })
-
-
 
 
 
