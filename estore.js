08a5b5bf0f8d9787d6eb4c227addb1fe225e7856
9adi3 <ahmadi.9adi3@gmail.com>
2025-06-23 16:58:01 +0100

    Update estore.js

--- estore.js
+++ estore.js
@@ -4,7 +4,10 @@ let category_nav_list = document.querySelector(".category_nav_list");
 function Open_Categ_list() {
   category_nav_list.classList.toggle("active") }
 
-
+let nav_links = document.querySelector(".nav_links")
+function open_Menu(){
+    nav_links.classList.toggle("active")
+  }
 
 
 var cart = document.querySelector('.cart');
@@ -52,24 +55,65 @@ function updateCart(){
 
   const cart = JSON.parse(localStorage.getItem('cart')) || []
 
+  var total_price =0
+  var total_count = 0
+
   cartItemsContainer.innerHTML = "";
-  cart.forEach((item , index) => {
+  cart.forEach((item , index) => {    
+
+    let total_Price_item = item.price * item.quantity;
+    total_price += total_Price_item
+    total_count += item.quantity
+
     cartItemsContainer.innerHTML +=`
+
     <div class="item_cart">
         <img src="${item.img}" alt="">
         <div class="content">
           <h4>${item.name}</h4>
-          <p class="price_cart">$${item.price}</p>
+          <p class="price_cart">$${total_Price_item}</p>
           <div class="quantity_control">
-            <button class="decrease_quantity">-</button>
+            <button class="decrease_quantity" data-index=${index}>-</button>
             <span class="quantity">${item.quantity}</span>
-            <button class="increase_quantity">+</button>
+            <button class="increase_quantity" data-index=${index}>+</button>
           </div>
         </div>
+
         <button class="delete_item" data-index="${index}"> <i class="fa-solid fa-trash-can"></i></button>
       </div>
     `
   })
+
+  const price_cart_total = document.querySelector(".price_cart_total")
+
+  const count_item_cart= document.querySelector(".Count_item_cart")
+
+  const count_item_header = document.querySelector(".count_item_header")
+
+  price_cart_total.innerHTML = `$ ${total_price}`
+  count_item_cart.innerHTML =  total_count
+  count_item_header.innerHTML =  total_count
+
+
+  const increaseButtons = document.querySelectorAll(".increase_quantity")
+  const decreaseButtons = document.querySelectorAll(".decrease_quantity")
+
+  increaseButtons.forEach(button => {
+    button.addEventListener("click" , (event) =>{
+      const itemIndex = event.target.getAttribute("data-index")
+      increaseQuantity(itemIndex)
+    })
+  })
+
+
+    decreaseButtons.forEach(button => {
+    button.addEventListener("click" , (event) =>{
+      const itemIndex = event.target.getAttribute("data-index")
+      decreaseQuantity(itemIndex)
+    })
+  })
+
+
 
   const deleteButtons = document.querySelectorAll('.delete_item')
 
@@ -81,6 +125,26 @@ function updateCart(){
     })
   })
 }
+
+function increaseQuantity(index){
+  let cart = JSON.parse(localStorage.getItem('cart')) || []
+  cart[index].quantity += 1
+  localStorage.setItem('cart' , JSON.stringify(cart))
+  updateCart() 
+}
+function decreaseQuantity(index){
+  let cart = JSON.parse(localStorage.getItem('cart')) || []
+
+  if (cart[index].quantity > 1){
+  cart[index].quantity -= 1}
+
+  localStorage.setItem('cart' , JSON.stringify(cart))
+  updateCart() 
+}
+
+
+
+
 
 function removeFromCart(index){
   const cart = JSON.parse(localStorage.getItem('cart')) || []
