bae54d5c8bd6010e42528ed5128faab23404a84e
9adi3 <ahmadi.9adi3@gmail.com>
2025-07-03 15:04:59 +0100

    Update estore_items.js

--- estore_items.js
+++ estore_items.js
@@ -1,25 +1,25 @@
-fetch('products.json')
-.then(response => response.json())
-.then(data => {
-    const cart = JSON.parse(localStorage.getItem('cart')) || []
+fetch("products.json")
+  .then((response) => response.json())
+  .then((data) => {
+    const cart = JSON.parse(localStorage.getItem("cart")) || [];
 
-    const swiper_items_sale = document.getElementById("swiper_items_sale")
+    const swiper_items_sale = document.getElementById("swiper_items_sale");
 
-    const swiper_electronics = document.getElementById("swiper_electronics")
+    const swiper_electronics = document.getElementById("swiper_electronics");
 
-    const swiper_appliances = document.getElementById("swiper_appliances")
+    const swiper_appliances = document.getElementById("swiper_appliances");
 
-    const swiper_mobiles = document.getElementById("swiper_mobiles")
+    const swiper_mobiles = document.getElementById("swiper_mobiles");
 
+    data.forEach((product) => {
+      if (product.old_price) {
+        const isInCart = cart.some((cartItem) => cartItem.id === product.id);
 
-    data.forEach(product => {
-        if(product.old_price){
+        const percent_disc = Math.floor(
+          ((product.old_price - product.price) / product.old_price) * 100
+        );
 
-            const isInCart =cart.some(cartItem => cartItem.id === product.id)
-
-            const percent_disc =Math.floor((product.old_price - product.price) / product.old_price * 100)
-                        
-            swiper_items_sale.innerHTML += `
+        swiper_items_sale.innerHTML += `
                 <div class="swiper-slide product">
                     <span class="sale_present">%${percent_disc}</span>
 
@@ -43,29 +43,37 @@ fetch('products.json')
                     </div>
                         
                     <div class="icons">
-                        <span class="btn_add_cart ${isInCart ? 'active' : ''}" data-id="${product.id}">
-                            <i class="fa-solid fa-cart-shopping"></i>  ${isInCart ? 'Item in cart' : 'Add to cart'}
+                        <span class="btn_add_cart ${
+                          isInCart ? "active" : ""
+                        }" data-id="${product.id}">
+                            <i class="fa-solid fa-cart-shopping"></i>  ${
+                              isInCart ? "Item in cart" : "Add to cart"
+                            }
                         </span>
                         <span class="icon_product">
                             <i class="fa-regular fa-heart"></i>
                         </span>
                     </div>
                 </div>
-                `
-        }        
+                `;
+      }
     });
 
-    data.forEach(product => {
-        if(product.catetory == "electronics"){
-            
-            const isInCart =cart.some(cartItem => cartItem.id === product.id)
+    data.forEach((product) => {
+      if (product.catetory == "electronics") {
+        const isInCart = cart.some((cartItem) => cartItem.id === product.id);
 
-            const old_price_Prgraph = product.old_price ? `<p class="old_price">$${product.old_price}</p>`:"";
-            
-            const precent_disc_div = product.old_price ? `<span class="sale_present">%${Math.floor((product.old_price - product.price) / product.old_price * 100)}</span>`:"";
+        const old_price_Prgraph = product.old_price
+          ? `<p class="old_price">$${product.old_price}</p>`
+          : "";
 
+        const precent_disc_div = product.old_price
+          ? `<span class="sale_present">%${Math.floor(
+              ((product.old_price - product.price) / product.old_price) * 100
+            )}</span>`
+          : "";
 
-            swiper_electronics.innerHTML += `
+        swiper_electronics.innerHTML += `
                 <div class="swiper-slide product">
                     
 
@@ -89,29 +97,37 @@ fetch('products.json')
                     </div>
                         
                     <div class="icons">
-                        <span class="btn_add_cart ${isInCart ? 'active' : ''}" data-id="${product.id}">
-                            <i class="fa-solid fa-cart-shopping"></i>  ${isInCart ? 'Item in cart' : 'Add to cart'}
+                        <span class="btn_add_cart ${
+                          isInCart ? "active" : ""
+                        }" data-id="${product.id}">
+                            <i class="fa-solid fa-cart-shopping"></i>  ${
+                              isInCart ? "Item in cart" : "Add to cart"
+                            }
                         </span>
                         <span class="icon_product">
                             <i class="fa-regular fa-heart"></i>
                         </span>
                     </div>
                 </div>
-            `
-        }
-    })
+            `;
+      }
+    });
 
-    data.forEach(product => {
-        if(product.catetory == "appliances"){
- 
-            const isInCart =cart.some(cartItem => cartItem.id === product.id)
+    data.forEach((product) => {
+      if (product.catetory == "appliances") {
+        const isInCart = cart.some((cartItem) => cartItem.id === product.id);
 
-            const old_price_Prgraph = product.old_price ? `<p class="old_price">$${product.old_price}</p>`:"";
-            
-            const precent_disc_div = product.old_price ? `<span class="sale_present">%${Math.floor((product.old_price - product.price) / product.old_price * 100)}</span>`:"";
+        const old_price_Prgraph = product.old_price
+          ? `<p class="old_price">$${product.old_price}</p>`
+          : "";
 
+        const precent_disc_div = product.old_price
+          ? `<span class="sale_present">%${Math.floor(
+              ((product.old_price - product.price) / product.old_price) * 100
+            )}</span>`
+          : "";
 
-            swiper_appliances.innerHTML += `
+        swiper_appliances.innerHTML += `
                 <div class="swiper-slide product">
                     
 
@@ -135,30 +151,36 @@ fetch('products.json')
                     </div>
                         
                     <div class="icons">
-                        <span class="btn_add_cart ${isInCart ? 'active' : ''}" data-id="${product.id}">
-                            <i class="fa-solid fa-cart-shopping"></i>  ${isInCart ? 'Item in cart' : 'Add to cart'}
+                        <span class="btn_add_cart ${
+                          isInCart ? "active" : ""
+                        }" data-id="${product.id}">
+                            <i class="fa-solid fa-cart-shopping"></i>  ${
+                              isInCart ? "Item in cart" : "Add to cart"
+                            }
                         </span>
                         <span class="icon_product"><i class="fa-regular fa-heart"></i>
                         </span>
                     </div>
                 </div>
-            `
-        }
-    })    
+            `;
+      }
+    });
 
+    data.forEach((product) => {
+      if (product.catetory == "mobiles") {
+        const isInCart = cart.some((cartItem) => cartItem.id === product.id);
 
-    data.forEach(product => {
-        if(product.catetory == "mobiles"){  
+        const old_price_Pargrahp = product.old_price
+          ? `<p class="old_price">$${product.old_price}</p>`
+          : "";
 
-            const isInCart = cart.some(cartItem => cartItem.id === product.id)
+        const percent_disc_div = product.old_price
+          ? `<span class="sale_present">%${Math.floor(
+              ((product.old_price - product.price) / product.old_price) * 100
+            )}</span>`
+          : "";
 
-            const old_price_Pargrahp = product.old_price ? `<p class="old_price">$${product.old_price}</p>` : "";
-
-            const percent_disc_div = product.old_price ? `<span class="sale_present">%${Math.floor((product.old_price - product.price) / product.old_price * 100)}</span>` : "";
-
-
-
-            swiper_mobiles.innerHTML += `
+        swiper_mobiles.innerHTML += `
 
 
             <div class="swiper-slide product">
@@ -176,7 +198,9 @@ fetch('products.json')
                            <i class="fa-solid fa-star"></i>
                        </div>
 
-                       <p class="name_product"><a href="#">${product.name}</a></p>
+                       <p class="name_product"><a href="#">${
+                         product.name
+                       }</a></p>
 
                        <div class="price">
                            <p><span>$${product.price}</span></p>
@@ -184,8 +208,12 @@ fetch('products.json')
                        </div>
 
                        <div class="icons">
-                            <span class="btn_add_cart ${isInCart ? 'active' : ''}" data-id="${product.id}">
-                            <i class="fa-solid fa-cart-shopping"></i>  ${isInCart ? 'Item in cart' : 'Add to cart'}
+                            <span class="btn_add_cart ${
+                              isInCart ? "active" : ""
+                            }" data-id="${product.id}">
+                            <i class="fa-solid fa-cart-shopping"></i>  ${
+                              isInCart ? "Item in cart" : "Add to cart"
+                            }
                             </span>
                             <span class="icon_product"><i class="fa-regular fa-heart"></i>
                             </span>
@@ -195,14 +223,7 @@ fetch('products.json')
            
            
            
-           `
-
-
-
-        }
-    })
-    
-})
-
-
-
+           `;
+      }
+    });
+  });
